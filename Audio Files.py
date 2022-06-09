830727fd1fcf4ca6bf9bbc60946e8e5fdeedce42
lraab01 <106100314+lraab01@users.noreply.github.com>
2022-06-08 23:31:42 -0400

    Add files via upload

---
+++ Audio Files.py
@@ -0,0 +1,8 @@
+##### AUDIO FILES #####
+## go to package manager under tools
+## download playsound and pyobjc
+## make sure mp3 and program are in same file
+
+import playsound
+shoes = 'shoes.mp3'
+playsound.playsound(shoes)
