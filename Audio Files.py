c511510c8082f66ddb70922437e5e9a874d75aab
lraab01 <106100314+lraab01@users.noreply.github.com>
2022-06-20 22:54:03 -0400

    Delete Audio Files.py

--- Audio Files.py
+++
@@ -1,8 +0,0 @@
-##### AUDIO FILES #####
-## go to package manager under tools
-## download playsound and pyobjc
-## make sure mp3 and program are in same file
-
-import playsound
-shoes = 'shoes.mp3'
-playsound.playsound(shoes)
